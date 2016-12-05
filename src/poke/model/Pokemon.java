5bee629400800b17511e00ba6e4a35c4d91b4a7f
mattpow35 <mpow2231@740s-pl-2.local>
2016-12-05 09:30:50 -0700

    added methods to pokemon.java
    get/set
    toString
    getPokemonInfo
    getPokemontypes

--- src/poke/model/Pokemon.java
+++ src/poke/model/Pokemon.java
@@ -22,11 +22,78 @@ public abstract class Pokemon
 		String [] pokeTypes = new String[types.length];
 		for(int index = 0; index < types.length; index++)
 		{
+			String temp = types[index].getCanonicalName();
 			
+			pokeTypes[index] = temp;
 		}
 		
+		for (String current : pokeTypes)
+		{
+			String temp = current.replace(this.getClass().getPackage().getName() + ".", "");
+			pokemonTypes += temp + "\n";
+		}
 		
 		return pokemonTypes;
 	}
 
+	public String toString()
+	{
+		String description = "This is a pokemon.";
+		
+		return description;
+	}
+	
+	public String getPokemonInformation()
+	{
+		String pokemonInfo = "This pokemon is of type: " + this.getClass().getSimpleName();
+		
+		return pokemonInfo;
+	}
+	
+	public int getHealthPoints()
+	{
+		return healthPoints;
+	}
+
+	public void setHealthPoints(int healthPoints)
+	{
+		this.healthPoints = healthPoints;
+	}
+
+	public int getAttackPoints()
+	{
+		return attackPoints;
+	}
+
+	public void setAttackPoints(int attackPoints)
+	{
+		this.attackPoints = attackPoints;
+	}
+
+	public double getSpeed()
+	{
+		return speed;
+	}
+
+	public void setSpeed(double speed)
+	{
+		this.speed = speed;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getNumber()
+	{
+		return number;
+	}
+	
+	
 }
